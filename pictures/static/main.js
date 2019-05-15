819ac49203daeab64bd7da5caa8e160f412ef0f8
HabeshaQueen <adhoadhigal.com>
2019-05-15 14:19:15 +0300

    add copy link

--- pictures/static/main.js
+++ pictures/static/main.js
@@ -1,9 +1,23 @@
-function copyImage(value){
-    function handler(event){
-        event.clipboardData.setData('text/plain', value);
-        event.preventDefault();
-        document.removeEventListner('copy', handler, true);
-    }
-document.addEventListner('copy', handler, true);
-document.execCommand('copy');
-}
+// function copyImage(value) {
+//     function handler(event){
+//         event.clipboardData.setData('text/plain', value);
+//         event.preventDefault();
+//         document.removeEventListner('copy', handler, true);
+//     }
+// document.addEventListner('copy', handler, true);
+// document.execCommand('copy');
+// }
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("copy");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
